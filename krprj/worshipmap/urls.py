e08f4d53d0a3aed2dd31797758a155c7590fd3cc
Christoph Heer <christoph.heer@googlemail.com>
2012-08-18 20:27:49 +0200

    Add a only_in_dev view for some geodjango expirements with jquery getJSON

--- krprj/worshipmap/urls.py
+++ krprj/worshipmap/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from .views import WorshipMapView
+from .views import WorshipMapView, GetPlacesInBoxJSONView
 
 urlpatterns = patterns('worshipmap.views',
     url(r'^$', WorshipMapView.as_view(), name='worshipmap'),
+    url(r'^_get_places$', GetPlacesInBoxJSONView.as_view()),
 )
--- krprj/worshipmap/views.py
+++ krprj/worshipmap/views.py
@@ -1,4 +1,11 @@
+import json
+
+from django.http import HttpResponse
+from django.views.generic import View
 from django.views.generic.base import TemplateView
+from django.contrib.gis.geos import Point, Polygon
+
+from krprj.world.models import WorldBorder
 
 
 class WorshipMapView(TemplateView):
@@ -10,3 +17,24 @@ class WorshipMapView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(WorshipMapView, self).get_context_data(**kwargs)
         return context
+
+
+class GetPlacesInBoxJSONView(View):
+
+    def get(self, request, *args, **kwargs):
+        p1x, p1y, p2x, p2y = (
+            float(n) for n in request.GET.get('in_bbox').split(',')
+        )
+        p1 = Point(p1x, p1y, srid=900913)
+        p1.transform(4326)
+        p2 = Point(p2x, p2y, srid=900913)
+        p2.transform(4326)
+
+        visible_map = Polygon.from_bbox(
+            (p1.x, p1.y, p2.x, p2.y)
+        )
+
+        countries = WorldBorder.objects.filter(mpoly__intersects=visible_map)
+        return HttpResponse(json.dumps({
+            'countries': list(countries.values('name', 'iso2'))
+        }))
