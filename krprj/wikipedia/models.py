df4e6a148255d9fa5a755673368d50f9844f3f5d
Andreas Madsack <andreas@madflex.de>
2012-08-18 22:26:37 +0200

    models for wikipedia part

--- krprj/wikipedia/models.py
+++ krprj/wikipedia/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class KircheWikipedia(models.Model):
+    title = models.CharField(max_length=200)
+
+    infobox = models.TextField(blank=True, null=True, default=None)
+    contents = models.TextField(blank=True, null=True, default=None)
+
+    lon = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+
+    categories = models.ManyToManyField(CategoryWikipedia,
+                                        related_name='categories')
+
+    languages = models.ManyToManyField(LanguageWikipedia,
+                                       related_name='languages')
+
+    values = models.ManyToManyField(ValueStore,
+                                    related_name='values')
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return "%s" % self.title
+
+
+class CategoryWikipedia(models.Model):
+    name = models.CharField(max_length=50)
+    language =  models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return "%s [%s]" % (self.name, self.language)
+
+class LaguageWikipedia(models.Model):
+    name = models.CharField(max_length=10)
+    long_name =  models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+class ValueStore(models.Model):
+    key =  models.CharField(max_length=50, primary=True)
+    value = models.TextField(default='')
+
+    def __unicode__(self):
+        return "%s" % (self.key)
+    
+
