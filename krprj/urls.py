f58a6820b9787aa50a1fd8f372d7a9c521d5ab0b
Andreas Madsack <andreas@madflex.de>
2012-08-18 21:31:39 +0200

    add url for /api/

--- krprj/urls.py
+++ krprj/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api/', include('krprj.api.urls')),
+
 )
