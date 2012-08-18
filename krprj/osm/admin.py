4b6ff1062717f4c64ed4e8c7294004c1b64c3fed
Christoph Heer <christoph.heer@googlemail.com>
2012-08-19 01:26:07 +0200

    Better admin interface for KircheOSM model

--- krprj/osm/admin.py
+++ krprj/osm/admin.py
@@ -1,4 +1,24 @@
 from django.contrib.gis import admin
 from models import KircheOsm
 
-admin.site.register(KircheOsm, admin.OSMGeoAdmin)
+
+class KircheOsmAdmin(admin.OSMGeoAdmin):
+    list_display = ['id', 'osm_id', 'name', 'religion', 'denomination', 'lon',
+                    'lat']
+    list_filter = ['religion', 'denomination']
+    search_fields = ['name', 'osm_id']
+
+    readonly_fields = ['id', 'osm_id']
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'osm_id')
+        }),
+        ('Place informations', {
+            'fields': ('name', 'religion', 'denomination', 'addional_fields')
+        }),
+        ('Geo informations', {
+                'fields': ('lon', 'lat', 'mpoly', 'point')
+        }),
+    )
+
+admin.site.register(KircheOsm, KircheOsmAdmin)
