1f0e0bd09ef8e3866b1a4076f5fd80a116822830
Andreas Madsack <andreas@madflex.de>
2012-08-18 08:22:54 +0200

    activate admin and gis

--- krprj/krprj/settings.py
+++ krprj/krprj/settings.py
@@ -118,10 +118,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.gis',
 )
 
 # A sample logging configuration. The only tangible logging
