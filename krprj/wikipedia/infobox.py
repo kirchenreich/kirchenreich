fef2d445e9791d74c1daaff27309a57820546ef4
Ramon Klass <tier@schokokeks.org>
2012-08-18 14:31:53 +0200

    stupid markup is stupid (code for review)

--- krprj/wikipedia/infobox.py
+++ krprj/wikipedia/infobox.py
@@ -4,18 +4,29 @@ def parse_infobox(page):
     ibox = {}
     text = ''.join(page)
     current = ''
-    start = text.lower().find('{{infobox')
-    if start > -1:
-        count = 0
-        for pos, letter in enumerate(text[start::]):
-            if letter == '{':
-                count += 1
-            elif letter == '}':
-                count -= 1
-            if count == 0:
-                end = start + pos + 1
-                break
-        infobox = text[start:end].split('\n')
+    infobox = []
+    inside = False
+    for line in page:
+        if line.lower().strip().startswith('{{infobox'):
+            inside = True
+        if inside:
+            infobox.append(line)
+        if line.lower().strip().startswith('}}'):
+            break
+    
+    # WIKIPEDIA MARKUP SUCKS    
+    # start = text.lower().find('{{infobox')
+    # if start > -1:
+    #     count = 0
+    #     for pos, letter in enumerate(text[start::]):
+    #         if letter == '{':
+    #             count += 1
+    #         elif letter == '}':
+    #             count -= 1
+    #         if count == 0:
+    #             end = start + pos + 1
+    #             break
+    #     infobox = text[start:end].split('\n')
         for line in infobox:
             if line.strip().startswith('|') and '=' in line:
                 key = ''
