51ffff550aa02d029fb01b58d23dba51d7904105
Christoph Heer <christoph.heer@googlemail.com>
2012-08-18 10:44:04 +0200

    Add setup.py to install the kirchenreich package system/env wide

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='kirchenreich',
+    version='0.1',
+    url='http://kirchenreich.org',
+    license='',
+    author='Three Followers',
+    author_email='team@kirchenreich.org',
+    description='',
+    packages=['krprj'],
+    zip_safe=False,
+    include_package_data=True,
+)
