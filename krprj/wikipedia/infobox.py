dd0d6b3375979f68efe61003e64497abc835fdc2
Ramon Klass <tier@schokokeks.org>
2012-08-18 20:37:31 +0200

    infobox parser, hopefully good enough for realworld use

--- krprj/wikipedia/infobox.py
+++ krprj/wikipedia/infobox.py
@@ -7,12 +7,12 @@ def parse_infobox(page):
     infobox = []
     inside = False
     for line in page:
-        if line.lower().strip().startswith('{{infobox'):
-            inside = True
+        if line.lower().strip().strip('|') == '}}':
+            break
         if inside:
             infobox.append(line)
-        if line.lower().strip().startswith('}}'):
-            break
+        if line.lower().strip().startswith('{{infobox'):
+            inside = True
     
     # WIKIPEDIA MARKUP SUCKS    
     # start = text.lower().find('{{infobox')
@@ -27,15 +27,31 @@ def parse_infobox(page):
     #             end = start + pos + 1
     #             break
     #     infobox = text[start:end].split('\n')
-        for line in infobox:
-            if line.strip().startswith('|') and '=' in line:
-                key = ''
-                value = ''
-                elems = line.strip('|').split('=')
-                if len(elems) > 0:
-                    key = elems[0].strip()
-                    if len(elems) > 1:
-                        value = elems[1].strip()
-                    ibox[key] = value
+
+    # box = ''.join(infobox)
+    # print "Box:", box
+    # kv = box.split('|')[1:-1]
+    # for pair in kv:
+    #     elems = pair.split('=', 1)
+    #     if len(elems) > 0:
+    #         key = elems[0].strip()
+    #         if len(elems) > 1:
+    #             value = elems[1].strip()
+    #         ibox[key] = value
+        
+    key = ''
+    value = ''
+    for line in infobox:
+        if line.strip().startswith('|') and '=' in line: 
+            ibox[key] = value
+            key = ''
+            value = ''
+            elems = line.strip('|').split('=', 1)
+            if len(elems) > 0:
+                key = elems[0].strip()
+                if len(elems) > 1:
+                    value = elems[1].strip()
+        else:
+            value = value + line
     return ibox
 
