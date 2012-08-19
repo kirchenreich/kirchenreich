9dbbdcb5742def1fd9ef1941d7d9a26f511aac9c
Andreas Madsack <andreas@madflex.de>
2012-08-20 01:00:38 +0200

    one time is enough

--- krprj/urls.py
+++ krprj/urls.py
@@ -11,9 +11,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^api/', include('krprj.api.urls')),
-
 )
 
 urlpatterns += worshipmap_patterns
