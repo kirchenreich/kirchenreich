501d22455376cf785ea3f9c0580a4a9d4553fbdb
Andreas Madsack <andreas@madflex.de>
2012-08-20 01:22:50 +0200

    added wikipedia admin

---
+++ krprj/wikipedia/admin.py
@@ -0,0 +1,10 @@
+from django.contrib.gis import admin
+from models import KircheWikipedia
+
+class KircheWikipediaAdmin(admin.OSMGeoAdmin):
+    list_display = ['id', 'title', 'lon', 'lat']
+    search_fields = ['title']
+
+    readonly_fields = ['id']
+
+admin.site.register(KircheWikipedia, KircheWikipediaAdmin)
