3982dae4fbbdb8c5ced21b2d52d4d22d2bfaf866
Andreas Madsack <andreas@madflex.de>
2012-08-19 19:23:46 +0200

    import polygon based place of worships from osm

--- krprj/osm/load.py
+++ krprj/osm/load.py
@@ -37,4 +37,49 @@ def load_nodes(filename):
 def load_polygon(filename):
     """ imports the churches which are consisting of polygons.
     """
-    pass
+    # read all coordinates in memory
+    fp = codecs.open('krprj/osm/data/coords_merged.data', encoding='utf-8', mode='r')
+    coords = {}
+    for line in fp:
+        try:
+            d = json.loads(line)
+            coords[d.get('id')] = d
+        except:
+            print line
+    fp.close()
+    print("dict loaded")
+    fp = codecs.open('krprj/osm/data/ways.data', encoding='utf-8', mode='r')
+    for line in fp:
+        data = json.loads(line)
+
+        kosm, created = KircheOsm.objects.get_or_create(osm_id=data['id'])
+
+        dtags = data.get('tags')
+        if 'name' in dtags:
+            kosm.name = dtags['name']
+            del dtags['name']
+        if 'religion' in dtags:
+            kosm.religion = dtags['religion']
+            del dtags['religion']
+        if 'denomination' in dtags:
+            kosm.denomination = dtags['denomination']
+            del dtags['denomination']
+        del dtags['amenity']
+        kosm.addional_fields = json.dumps(dtags)
+
+        tpl = []
+        for ref in data.get('refs'):
+            x = coords.get(ref)
+            if x:
+                tpl.append(tuple([x.get('lon'), 
+                                  x.get('lat')]))
+            else:
+                print("missing: %d" % ref)
+
+        kosm.mpoly = MultiPolygon(Polygon(tuple(tpl)))
+        print kosm.mpoly
+
+        kosm.point = kosm.mpoly.centroid
+        print kosm.point
+
+        kosm.save()
