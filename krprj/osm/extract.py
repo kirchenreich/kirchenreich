0a22e8942a75aed118ffb88957f5c654af0d23ac
Andreas Madsack <andreas@madflex.de>
2012-08-19 13:25:27 +0200

    third level of extraction. get all missing ones (some maybe ways :( )

--- krprj/osm/extract.py
+++ krprj/osm/extract.py
@@ -94,23 +94,52 @@ class GetRefs(object):
                 self.osmid = self.nextref()
 
 
-class GetAllRefs(object):
-    """ Get all nodes for ways from first run.
+class GetRefsRemaining(object):
+    """ Get all nodes/ways for ways from first run/second.
     """
 
     def __init__(self, opts, fn=None):
-        self.fcoords = open(os.path.join(opts.datadir, "coords_all.data"), "w")
+        if not fn:
+            fn = os.path.join(opts.datadir, "missing_refs.data.sort")
+        self.loadrefs(fn)
+        self.fcoords = open(os.path.join(opts.datadir, "coords3.data"), "w")
+        self.fways = open(os.path.join(opts.datadir, "ways3.data"), "w")
+        self.frefs = open(os.path.join(opts.datadir, "refs3.data"), "w")
 
     def __del__(self):
         self.fcoords.close()
+        self.fways.close()
+        self.frefs.close()
+
+    def loadrefs(self, fn):
+        fp = open(fn, "r")
+        self.mrefs = []
+        for line in fp:
+            self.mrefs.append(int(line.strip()))
+        self.mrefs = set(self.mrefs)
+        fp.close()
 
     def coords(self, coords):
-        """ save all coords
+        """ save all coords to corresponding refs
         every line is one node as a dict (in json)
         """
         for osmid, lon, lat in coords:
-            d = {'id': osmid, 'lon':lon, 'lat':lat}
-            self.fcoords.write("%s\n" % json.dumps(d))
+            if osmid in self.mrefs:
+                d = {'id': osmid, 'lon':lon, 'lat':lat}
+                self.fcoords.write("%s\n" % json.dumps(d))
+                self.mrefs.remove(osmid)
+
+    def ways(self, ways):
+        """ save all ways to corresponding refs
+        every line is one way as a dict (in json)
+        """
+        for osmid, tags, refs in ways:
+            if osmid in self.mrefs:
+                d = {'id': osmid, 'tags':tags, 'refs':refs}
+                self.fcoords.write("%s\n" % json.dumps(d))
+                self.mrefs.remove(osmid)
+                for ref in refs:
+                    self.frefs.write("%d\n" % ref)
 
 
 def options():
@@ -159,7 +188,7 @@ if __name__=='__main__':
     if opts.alt:
         if opts.verbose:
             print("alternative run")
-        alt = GetAllRefs(opts)
+        alt = GetRefsRemaining(opts)
         p = OSMParser(concurrency=4,
                       coords_callback=alt.coords)
         p.parse(opts.filename)
