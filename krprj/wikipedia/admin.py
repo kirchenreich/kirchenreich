daa71c1c6d9fdbd33feb7e006110b7fd78f65250
Ramon Klass <tier@schokokeks.org>
2012-08-20 01:51:54 +0200

    pep8

--- krprj/wikipedia/admin.py
+++ krprj/wikipedia/admin.py
@@ -1,6 +1,7 @@
 from django.contrib.gis import admin
 from models import KircheWikipedia
 
+
 class KircheWikipediaAdmin(admin.OSMGeoAdmin):
     list_display = ['id', 'title', 'lon', 'lat', 'unite']
     search_fields = ['title']
