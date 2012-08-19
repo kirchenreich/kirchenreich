04de32c46496f472e1fddd20b7b6fff17fe0b285
Andreas Madsack <andreas@madflex.de>
2012-08-19 23:26:46 +0200

    correlate

--- krprj/krunite/models.py
+++ krprj/krunite/models.py
@@ -50,20 +50,30 @@ class KircheUniteManager(models.Manager):
         from krprj.world.models import WorldBorder
         from krprj.wikipedia.models import KircheWikipedia
 
-        for elem in KircheOsm.objects.all()[0:1]:
+        for elem in KircheOsm.objects.all():
             if not elem.unite:
                 elem.unite = KircheUnite.objects.create(name=elem.name,
                                                         point=elem.point)
-            elem.unite.country = WorldBorder.objects.get(
-                mpoly__intersects=elem.point)
+            try:
+                wb = WorldBorder.objects.get(mpoly__intersects=elem.point)
+                elem.unite.country = wb
+            except WorldBorder.DoesNotExist:
+                elem.unite.country = None
             elem.unite.save()
             if not elem.unite.checks:
                 chks = KircheChecks.objects.create(osm=True)
+            else:
+                chks = elem.unite.checks
             chks.run()
             elem.unite.checks = chks
 
             # find all wikipedia entries within 100 meters
-            # KircheWikipedia.objects.filter(point__distance_lte=(a.point, 100))
+            pnts = KircheWikipedia.objects.filter(
+                point__distance_lte=(elem.point, 100))
+            if pnts:
+                for pnt in pnts:
+                    elem.unite.kirchewikipedia_set.add(pnt)
+                print elem.unite.kirchewikipedia_set.all()
 
             elem.unite.save()
             elem.save()
