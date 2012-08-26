fe889ceb490d43b7ef137e9b8785fda38bb2f7dc
Andreas Madsack <andreas@madflex.de>
2012-08-26 14:14:58 +0200

    add new osm_type to admin

--- krprj/osm/admin.py
+++ krprj/osm/admin.py
@@ -4,14 +4,14 @@ from models import KircheOsm
 
 class KircheOsmAdmin(admin.OSMGeoAdmin):
     list_display = ['id', 'osm_id', 'name', 'religion', 'denomination', 'lon',
-                    'lat']
+                    'lat', 'osm_type']
     list_filter = ['religion', 'denomination']
     search_fields = ['name', 'osm_id']
 
-    readonly_fields = ['id', 'osm_id']
+    readonly_fields = ['id', 'osm_id', 'osm_type']
     fieldsets = (
         (None, {
-            'fields': ('id', 'osm_id')
+            'fields': ('id', 'osm_id', 'osm_type')
         }),
         ('Place informations', {
             'fields': ('name', 'religion', 'denomination', 'addional_fields')
