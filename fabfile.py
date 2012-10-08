63cf00259881c19b0727292a2f990726f2015acd
Christoph Heer <Christoph.Heer@gmail.com>
2012-10-08 17:47:21 +0200

    Fix proces name of celery worker process

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from fabric.api import task, env, sudo, cd, prefix
 from fabric.utils import puts
@@ -78,7 +79,9 @@ def restart_celery():
     """Restart the celery worker"""
     puts(yellow("Restart celery worker"))
     with prefix('source %s' % in_rwd('bin/activate')):
-        sudo('supervisorctl restart celery', user=env.app_user)
+        sudo('supervisorctl restart celery-worker', user=env.app_user)
+        time.sleep(1)
+        sudo('supervisorctl status', user=env.app_user)
 
 
 @task
