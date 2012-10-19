9013f8fd3a13a50e9b25e34bed85f8c022b8d452
Andreas Madsack <andreas.madsack@ims.uni-stuttgart.de>
2012-10-19 19:30:36 +0200

    statistics view first version

--- krprj/worshipmap/views.py
+++ krprj/worshipmap/views.py
@@ -5,7 +5,11 @@ from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 
 from krprj.osm.models import KircheOsm
+from krprj.osm.models import Ref
 from krprj.wikipedia.models import KircheWikipedia
+
+import datetime
+from django.utils.timezone import utc
 
 
 class WorshipMapView(TemplateView):
@@ -47,5 +51,38 @@ class PlaceOfWorshipDetailView(DetailView):
             context['checks'] = self.object.unite.checks
             context['wikipedia_pages'] = self.object.unite\
                                                     .kirchewikipedia_set.all()
+
+        return context
+
+class StatisticsView(TemplateView):
+    """Show some stats about the data"""
+
+    template_name = "statistics.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(StatisticsView, self).get_context_data(
+            **kwargs)
+
+        # Ref counts
+        context['ref_need_update'] = Ref.objects.filter(need_update=True).count()
+        context['ref_count'] = Ref.objects.count()
+
+        # osm_types
+        data = [i[0] for i in KircheOsm.objects.values_list('osm_type')]
+        uniques = set(data)
+        data = dict([(i, data.count(i)) for i in uniques])
+        context['osm_type'] = data
+
+        # date
+        context['older_than_1week'] = KircheOsm.objects.filter(last_update__lt=(
+                datetime.datetime.utcnow().replace(tzinfo=utc) -
+                datetime.timedelta(days=7))).count()
+        context['older_than_2weeks'] = KircheOsm.objects.filter(last_update__lt=(
+                datetime.datetime.utcnow().replace(tzinfo=utc) -
+                datetime.timedelta(days=14))).count()
+        context['older_than_3weeks'] = KircheOsm.objects.filter(last_update__lt=(
+                datetime.datetime.utcnow().replace(tzinfo=utc) -
+                datetime.timedelta(days=21))).count()
 
         return context
