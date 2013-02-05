6691d91881aaf15356afedc669637e6ae0ee520b
Christoph Heer <christoph.heer@googlemail.com>
2013-02-05 12:43:05 +0100

    Start rewrite of statistics view to dashboard view

--- krprj/worshipmap/urls.py
+++ krprj/worshipmap/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import WorshipMapView, PlaceOfWorshipDetailView, StatisticsView
+from .views import WorshipMapView, PlaceOfWorshipDetailView, DashboardView
 
 urlpatterns = patterns('worshipmap.views',
-    url(r'^statistics', StatisticsView.as_view()),
+    url(r'^dashboard', DashboardView.as_view()),
     url(r'^(?P<pk>\d+)', PlaceOfWorshipDetailView.as_view()),
     url(r'^$', WorshipMapView.as_view(), name='worshipmap'),
 )
--- krprj/worshipmap/views.py
+++ krprj/worshipmap/views.py
@@ -1,8 +1,8 @@
 import json
-from copy import copy
 
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
+from django.db.models import Count
 
 from krprj.krunite.models import KircheUnite
 from krprj.osm.models import KircheOsm, Ref
@@ -61,38 +61,51 @@ class PlaceOfWorshipDetailView(DetailView):
         return context
 
 
-class StatisticsView(TemplateView):
+class DashboardView(TemplateView):
     """Show some stats about the data"""
 
-    template_name = "statistics.html"
+    template_name = "dashboard.html"
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
-        context = super(StatisticsView, self).get_context_data(
-            **kwargs)
+        context = super(DashboardView, self).get_context_data(**kwargs)
+
+        context['wikipedia_count'] = KircheWikipedia.objects.count()
+        context['osm_count'] = KircheOsm.objects.count()
+        context['krunite_count'] = KircheUnite.objects.count()
 
         # Ref counts
-        context['ref_need_update'] = Ref.objects.filter(need_update=True).count()
+        context['ref_need_update'] = Ref.objects.filter(need_update=True) \
+                                                .count()
         context['ref_count'] = Ref.objects.count()
 
         # osm_types
-        data = [i[0] for i in KircheOsm.objects.values_list('osm_type')]
-        uniques = set(data)
-        data = dict([(i, data.count(i)) for i in uniques])
-        context['osm_type'] = data
+        context['osm_type'] = KircheOsm.objects.values_list('osm_type') \
+                                               .annotate(count=Count("id"))
+        context['osm_type'] = dict(context['osm_type'])
 
         # date
-        context['last_7days'] = KircheOsm.objects.filter(last_update__gt=(
+        context['last_7days'] = KircheOsm.objects.filter(
+            last_update__gt=(
                 datetime.datetime.utcnow().replace(tzinfo=utc) -
-                datetime.timedelta(days=7))).count()
-        context['older_than_1week'] = KircheOsm.objects.filter(last_update__lt=(
+                datetime.timedelta(days=7)
+            )
+        ).count()
+        context['older_than_1week'] = KircheOsm.objects.filter(
+            last_update__lt=(
                 datetime.datetime.utcnow().replace(tzinfo=utc) -
-                datetime.timedelta(days=7))).count()
+                datetime.timedelta(days=7)
+            )
+        ).count()
         context['older_than_2weeks'] = KircheOsm.objects.filter(last_update__lt=(
                 datetime.datetime.utcnow().replace(tzinfo=utc) -
-                datetime.timedelta(days=14))).count()
+                datetime.timedelta(days=14)
+            )
+        ).count()
         context['older_than_3weeks'] = KircheOsm.objects.filter(last_update__lt=(
                 datetime.datetime.utcnow().replace(tzinfo=utc) -
-                datetime.timedelta(days=21))).count()
+                datetime.timedelta(days=21)
+            )
+        ).count()
 
         return context
